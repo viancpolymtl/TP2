a499418b034835dc4ef3f7c16553aedeb2f41245
AlexxPC <alexandre.provostcardin@gmail.com>
2023-09-18 08:51:54 -0400

    First commit

---
+++ part1.c
@@ -0,0 +1,20 @@
+/*
+ * Ecole polytechnique de Montreal - GIGL
+ * Automne  2022
+ * Initlab - part1.c
+ * 
+ * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
+ */
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion et les déclarations globales
+// -------------------------------------------------
+
+   
+// -------------------------------------------------
+
+int main () {
+    // TODO
+    return 0;
+}
+    
